1f9a373d320748b91d1007ac139a0b7982dbe180
Raildex <n@a>
2022-07-12 13:31:17 +0200

    Added Output

--- src/main.cpp
+++ src/main.cpp
@@ -14,11 +14,14 @@ int main(int argc, char** argv) {
 					 "[path/to/out_file] [namespace] -- converts a file to a C++ header with a "
 					 "namespace\n";
 		std::cout << std::flush;
+		return 0;
 	}
 	if(argc == 5) {
 		if(!strcmp(argv[1], "-c")) {
 			std::filesystem::path out_file = argv[4];
-			auto converter = rdx::f2c::c_converter{.variable_name = argv[3], .bytes = rdx::f2c::read_file_to_byte_array(argv[2]), .out_file = out_file};
+			auto bytes = rdx::f2c::read_file_to_byte_array(argv[2]) ;
+			std::cout << "Byte Count: " << bytes.size();
+			auto converter = rdx::f2c::c_converter{.variable_name = argv[3], .bytes = bytes, .out_file = out_file};
 			auto out_src_stream = std::ofstream(out_file.replace_extension("c"));
 			auto out_header_stream = std::ofstream(out_file.replace_extension("h"));
 			out_src_stream << converter.get_source_content();
@@ -28,7 +31,9 @@ int main(int argc, char** argv) {
 	} else if(argc == 6) {
 		if(!strcmp(argv[1], "-cpp")) {
 			std::filesystem::path out_file = argv[4];
-			auto converter = rdx::f2c::cpp_converter{.variable_name = argv[3], .bytes = rdx::f2c::read_file_to_byte_array(argv[2]), .out_file = out_file , .name_space = argv[5]};
+			auto bytes = rdx::f2c::read_file_to_byte_array(argv[2]);
+			std::cout << "Byte Count: " << bytes.size();
+			auto converter = rdx::f2c::cpp_converter{.variable_name = argv[3], .bytes = bytes, .out_file = out_file, .name_space = argv[5]};
 			auto out_src_stream = std::ofstream(out_file.replace_extension("cpp"));
 			auto out_header_stream = std::ofstream(out_file.replace_extension("hpp"));
 			out_src_stream << converter.get_source_content();
